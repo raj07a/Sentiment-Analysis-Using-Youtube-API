4132f26931d9ba5ac7606f34da6db816cb13f413
RAJA RAWAT <122098595+raj07a@users.noreply.github.com>
2024-09-18 14:07:38 +0530

    api.py

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ from textblob import TextBlob
 from wordcloud import WordCloud
 
 # API key and channel ID (Replace with your YouTube API Key and Channel ID)
-API_KEY = "AIzaSyBY6kSnQUuTrlT91zzEAF_5QZZHaqtMM8I"
+API_KEY = "AIzaSyAdQav-iU4TAW9r9LIBYPl2l3-VcZjJslU"
 CHANNEL_ID = "UCDDjMFHTsEerSEm2BvhcwrA"
 
 # Function to fetch YouTube video data
